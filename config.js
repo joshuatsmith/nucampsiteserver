d7b3ff3ddfd22beb3a5082e9ef618c56a6f203c2
joshuaTsmith <josh.tlee.smith@gmail.com>
2021-01-26 10:50:42 -0800

    OAuth FACEBOOK

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '736116930422706',
+        clientSecret: '71e56e0e5ea08cd9eee7faf79c034d38'
+    }
 }
